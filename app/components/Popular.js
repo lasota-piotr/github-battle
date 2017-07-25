62fb18c842747a814fb8c5ffd021227d0ca55a8d
Piotr Lasota <lasota.piotr1@gmail.com>
2017-07-25 21:39:27 +0200

    added displaying repos list

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,11 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import api from '../utils/api';
 
+const RepoGrid = (props) => {
+  return (
+    <ul className="o-flex o-flex--justify-content-around o-flex--wrap o-list-bare">
+      {
+        props.repos.map((repo, index) => (
+          <li
+            key={repo.name}
+            className="c-repo-item">
+            <div className="u-text-center c-repo-item__prominent-text">#{index + 1}</div>
+            <ul className="o-list-bare o-flex o-flex--column o-flex--align-items-center u-margin-bottom-large">
+              <li>
+                <img
+                  src={repo.owner.avatar_url}
+                  alt={`Avatar ${repo.name}`}
+                  className="c-avatar"
+                />
+              </li>
+              <li>
+                <a href={repo.html_url}><h3 className="c-repo-item__title">{repo.name}</h3></a>
+              </li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        ))
+      }
+    </ul>
+  )
+};
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+};
+
 const SelectLanguage = props => {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
   return (
     <ul className="o-list-bare
-                   c-page-head">
+                   c-page-head
+                   u-margin-top u-margin-bottom-large">
       {
         languages.map(language => (
             <li key={language}
@@ -62,7 +97,13 @@ export default class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
         />
-        {JSON.stringify(this.state.repos)}
+        {
+          !!this.state.repos ? (
+            <RepoGrid repos={this.state.repos}/>
+          ) : (
+            <div className="o-flex o-flex--justify-content-center u-margin-top">Loading...</div>
+          )
+        }
       </div>
     )
   }
