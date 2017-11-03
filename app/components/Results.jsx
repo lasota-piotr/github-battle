24da0ebf7ac5ba89b339980db54fe2a89d1da80f
Piotr Lasota <lasota.piotr1@gmail.com>
2017-11-03 21:57:25 +0100

    Add result of battle fetching - work in progress

--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import queryString from 'query-string';
+import { battle } from '../utils/api';
 
 class Results extends React.Component {
+  componentDidMount() {
+
+  }
+
   render() {
+    const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
+    battle([playerOneName, playerTwoName]).then((players) => { console.log(players); });
     return (
       <div>
         <h1>Results</h1>
-        <div>{this.props.location.search}</div>
+        <div>{playerOneName},{playerTwoName}</div>
       </div>
     );
   }
--- app/utils/api.js
+++ app/utils/api.js
@@ -4,7 +4,7 @@ const getProfile = username =>
   axios.get(`https://api.github.com/users/${username}`).then(user => user.data);
 
 const getRepos = username =>
-  axios.get(`https://api.github.com/users/${username}/repos&per_page=100`);
+  axios.get(`https://api.github.com/users/${username}/repos`);
 
 const getStarCount = repos =>
   repos.data.reduce((acc, repo) => acc + repo.stargazers_count, 0);
@@ -35,8 +35,6 @@ export const battle = players =>
     .catch(handleError);
 
 export const fetchPopularRepos = (language) => {
-  const encodedURI = window.encodeURI(
-    `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`,
-  );
+  const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
   return axios.get(encodedURI).then(response => response.data.items);
 };
