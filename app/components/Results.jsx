8b07e916894ff87ae872c06821830c4800edf08e
Piotr Lasota <lasota.piotr1@gmail.com>
2017-11-04 20:35:05 +0100

    Add error handling for fetching results

--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -1,23 +1,72 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import queryString from 'query-string';
 import { battle } from '../utils/api';
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      winner: null,
+      loser: null,
+      loading: true,
+      error: null,
+    };
+  }
   componentDidMount() {
+    const errorHandle = (error) => {
+      this.setState({
+        loading: false,
+        error,
+      });
+    };
 
+    const {
+      playerOneName,
+      playerTwoName,
+    } = queryString.parse(this.props.location.search);
+    battle([playerOneName, playerTwoName])
+      .then(([winner, loser]) => {
+        if (!winner || !loser) {
+          errorHandle('Oops... There is a problem with fetch results.');
+        }
+        this.setState({
+          loading: false,
+          winner,
+          loser,
+        });
+      })
+      .catch(() => {
+        errorHandle('Oops... There is a problem with fetch results.');
+      });
   }
 
   render() {
-    const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
-    battle([playerOneName, playerTwoName]).then((players) => { console.log(players); });
+    const {
+      winner, loser, loading, error,
+    } = this.state;
+    console.log(winner, loser);
     return (
       <div>
-        <h1>Results</h1>
-        <div>{playerOneName},{playerTwoName}</div>
+        <h1 className="c-title u-text-center u-margin-top">Results</h1>
+        {loading && (
+          <div>
+            <h3 className="u-text-center u-margin-top">Loading...</h3>
+          </div>
+        )}
+        {error && (
+          <div>
+            <p className="u-text-center u-margin-top">
+              {error}
+            </p>
+            <div className="u-text-center u-margin-top">
+              <Link to="/battle"><button className="c-button">Reset</button></Link>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
 }
-
 
 export default Results;
